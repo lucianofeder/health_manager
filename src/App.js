bad472a3602471d23558dfaf623363d52d676c68
Luciano Feder <lucianoo.feder@gmail.com>
2021-03-10 19:12:58 +0000

    fix: excluding elements from app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Button from "./components/Button";
 import Routes from "./routes";
 
 const App = () => {
   return (
     <div className="App">
       <Routes />
-      <Button/>
     </div>
   );
 };
