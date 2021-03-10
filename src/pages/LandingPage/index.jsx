3ceb1a3e4b7d8d52bac480f35384d25449dd8bd8
Matheus Dantas <correia.dantas@outlook.com>
2021-03-10 17:56:06 -0300

    feat: estrutura base para receber o button

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,10 +1,29 @@
-import { Container } from "./style";
+import { Container, Div, LogoStyled, DivStyled, DivAjust } from "./style";
+import landingPage from "../../images/Logo/landingPage.svg";
+import logo1 from "../../images/Logo/logo1.png";
+import clouds from "../../images/clouds.svg";
 
 const LandingPage = () => {
   return (
-    <Container>
-      <h1>LandingPage</h1>
-    </Container>
+    <>
+      <Container>
+        <DivAjust>
+          <Div large order="1">
+            <LogoStyled src={landingPage} />
+          </Div>
+
+          <DivStyled order="" internalOrder="1">
+            <Div medium>
+              <LogoStyled medium src={logo1} />
+            </Div>
+            <Div small>Habitualmente Conectados</Div>
+            <Div>@components/Button</Div>
+          </DivStyled>
+        </DivAjust>
+
+        <LogoStyled order="2" fullWidth src={clouds} />
+      </Container>
+    </>
   );
 };
 
--- src/pages/LandingPage/style.js
+++ src/pages/LandingPage/style.js
@@ -4,11 +4,11 @@ import Colors from "../../global/Colors";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: space-between;
 
-  min-height: 630px;
-  max-height: 660px;
+  min-height: 680px;
 
   background: ${Colors.BackgroundLinear};
   font-family: Montserrat;
@@ -18,4 +18,159 @@ export const Container = styled.div`
   color: ${Colors.BasePurple};
   overflow-x: hidden;
   overflow-y: hidden;
+
+  /* Tablet */
+  @media (min-width: 700px) {
+    font-size: 35px;
+    min-height: 635px;
+  }
+
+  /* Desktop */
+  @media (min-width: 1020px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    font-size: 45px;
+
+    &:nth-child(3) {
+      order: 2;
+    }
+  }
+
+  /* 4K */
+  @media (min-width: 1920px) {
+    font-size: 55px;
+    max-height: 1080;
+  }
+`;
+
+export const Div = styled.div`
+  order: ${(props) => props.order};
+
+  width: 100vw;
+
+  padding: ${(props) =>
+    props.small
+      ? "1em 0 1em 0"
+      : props.medium
+      ? "1.5em 0 1em 0"
+      : props.large
+      ? "0"
+      : "0.5em"};
+
+  max-height: ${(props) =>
+    props.small
+      ? "70px"
+      : props.medium
+      ? "120px"
+      : props.large
+      ? "220px"
+      : "50px"};
+
+  min-height: ${(props) =>
+    props.small
+      ? "40px"
+      : props.medium
+      ? "100px"
+      : props.large
+      ? "220px"
+      : "40px"};
+
+  @media (min-width: 700px) {
+    padding: 0.3em;
+
+    min-height: ${(props) =>
+      props.small
+        ? "40px"
+        : props.medium
+        ? "100px"
+        : props.large
+        ? "620px"
+        : "40px"};
+  }
+
+  @media (min-width: 1020px) {
+    padding: 0.5em;
+
+    width: ${(props) =>
+      props.small
+        ? "550px"
+        : props.medium
+        ? "550px"
+        : props.large
+        ? "600px"
+        : "450px"};
+
+    min-height: ${(props) =>
+      props.small
+        ? "40px"
+        : props.medium
+        ? "100px"
+        : props.large
+        ? "420px"
+        : "40px"};
+  }
+`;
+
+export const DivStyled = styled.div`
+  order: ${(props) => props.order};
+  padding: 0.2em;
+  width: 100%;
+
+  @media (min-width: 700px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 40vh;
+  }
+
+  @media (min-width: 1020px) {
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    order: ${(props) => props.internalOrder};
+  }
+`;
+
+export const DivAjust = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 98vw;
+  padding-top: 1em;
+  height: 58vh;
+  align-items: center;
+
+  @media (min-width: 1020px) {
+    flex-direction: row;
+    justify-content: space-around;
+
+    &:nth-child(2) {
+      order: 1;
+    }
+  }
+`;
+
+export const LogoStyled = styled.img`
+  order: ${(props) => props.order};
+  width: ${(props) =>
+    props.small
+      ? "200px"
+      : props.medium
+      ? "300px"
+      : props.fullWidth
+      ? "100%"
+      : ""};
+
+  @media (min-width: 1020px) {
+    width: ${(props) =>
+      props.small
+        ? "200px"
+        : props.medium
+        ? "400px"
+        : props.fullWidth
+        ? "100%"
+        : "600px"};
+  }
 `;
