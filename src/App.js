4c23b294beb49833b822af4b37da4ae5b2f31e10
Luciano Feder <lucianoo.feder@gmail.com>
2021-03-11 01:30:47 +0000

    fix: removed unused elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Routes from "./routes";
-import Input from "./components/Input";
 
 const App = () => {
   return (
     <div className="App">
       <Routes />
-      <Input placeholder="teste" name="teste" />
     </div>
   );
 };
