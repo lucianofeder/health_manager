21eb18141f51e50772b6bce4b1c0b072e6609ece
Jardel lacerda <jardellacerda2001@gmail.com>
2021-03-15 21:20:18 +0000

    fix(@component/Card): att component, created instructions for new props and change format

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,52 @@
+/*
+Todo - Instruções
+
+?Passar esses componentes por props: add, title, modalAdd e modalEdit
+
+!--------------
+*title - format: string - titulo do card
+*example - "Grupos"
+*to use - title="Grupos"
+!--------------
+*add - format: boleano - permição para exibir botão adicionar
+*example - true
+*to use - add={true}
+!--------------
+*modalAdd - format: component - Componente a ser usado ao adicionar
+*example - <CreateGroup/>
+*to use - modalAdd={<CreateGroup/>}
+!--------------
+*modalEdit - format: component - Componente a ser usado ao editar
+*example - <EditGroup/>
+*to use - modalEdit={<EditGroup/>}
+
+?passar esse componente por children: children
+
+*children - format: component - Componente a ficar dentro do card
+*example - <p>Test</p>;
+*to use - <Card>  
+*            <p>Test</p> 
+*          </Card>
+
+*/
+
 import { CardContainer } from "./style";
-import Edit from "../../images/pen.svg";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Card = (props) => {
-  const { children, add, title } = props;
+  const { id } = useParams();
+  const id_user = useSelector((state) => state.users.user_id);
+
+  const { children, add, title, modalAdd, modalEdit } = props;
   return (
     <CardContainer>
-      <div className="iconModal">
-        {add && <h1>+</h1>}
-        <img src={Edit} alt="edit" />
-      </div>
+      {id === id_user && (
+        <div className="iconModal">
+          {add && modalAdd}
+          {modalEdit}
+        </div>
+      )}
 
       <div>
         <h1>{title}</h1>
